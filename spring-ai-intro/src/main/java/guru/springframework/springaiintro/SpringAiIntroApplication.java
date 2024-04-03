0745a368544b9094154316c6cd513daf641115eb
John Thompson <john@springframework.guru>
2024-04-03 11:59:20 -0400

    initial project create

---
+++ spring-ai-intro/src/main/java/guru/springframework/springaiintro/SpringAiIntroApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.springaiintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiIntroApplication.class, args);
+    }
+
+}
---
+++ spring-ai-intro/src/test/java/guru/springframework/springaiintro/SpringAiIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springaiintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
